5a19dc26e618c0a9a17a92945e1c816e1c635e0f
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-02-06 23:33:38 -0800

    Meetup producer

--- src/main/java/MeetupRSVPProducer.java
+++ src/main/java/MeetupRSVPProducer.java
@@ -1,6 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+
 public class MeetupRSVPProducer {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
+//        ws://stream.meetup.com/2/rsvps
+        URL url = new URL("https://stream.meetup.com/2/rsvps");
+      InputStream in = url.openStream();
+//
+//        BufferedReader Kn = new BufferedReader(
+//                new InputStreamReader(in,"UTF-8"));
+//
+
+        JsonFactory factory = new JsonFactory();
+        JsonParser jParser = factory.createParser(in);
+//        String line = Kn.readLine();
+        while (!jParser.isClosed()) {
+            System.out.println(jParser.readValueAsTree());
+
+        }
+
 
     }
+
+
+
+
 }
