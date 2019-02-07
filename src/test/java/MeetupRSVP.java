441a99b59ec3619d7c718120f633e55b6dad503d
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-02-07 00:41:37 -0800

    Meetup producer

--- src/test/java/MeetupRSVP.java
+++ src/test/java/MeetupRSVP.java
@@ -1,24 +1,23 @@
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
 import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.URL;
 
-public class MeetupRSVPProducer {
 
+public class MeetupRSVP {
     public static void main(String[] args) throws Exception{
 //        ws://stream.meetup.com/2/rsvps
         URL url = new URL("https://stream.meetup.com/2/rsvps");
-      InputStream in = url.openStream();
+        InputStream in = url.openStream();
+
 //
-//        BufferedReader Kn = new BufferedReader(
-//                new InputStreamReader(in,"UTF-8"));
+        InputStreamReader Kn = new InputStreamReader(in,"UTF-8");
+//        BufferedReader bn = new BufferedReader(kn);
+//        String line;
+//        while(line =)
 //
 
 //        JsonFactory factory = new JsonFactory();
@@ -30,18 +29,17 @@ public class MeetupRSVPProducer {
 //        }
 
 
-            Gson json = new Gson();
-            JsonReader parser = new JsonReader(new InputStreamReader(in));
-                      parser.setLenient(true);
-                     String a =  json.fromJson(parser,String.class);
-                     System.out.println(a);
+//        Gson json = new Gson();
+//        JsonReader parser = new JsonReader(new InputStreamReader(in));
+//        parser.setLenient(true);
+//        String a =  json.fromJson(parser,String.class);
+//        System.out.println(a);
 //
 //            JsonElement rootelement =  parser.
 //            JsonObject rootobject = rootelement.getAsJsonObject();
 
 
     }
-
 
 
 
