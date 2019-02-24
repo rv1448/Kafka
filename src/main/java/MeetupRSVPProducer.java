aa6547ecf3fde7d889f4591d4edbf13400a7f83d
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-02-24 15:39:38 -0800

    Meetup producer JSON Stream

--- src/main/java/MeetupRSVPProducer.java
+++ src/main/java/MeetupRSVPProducer.java
@@ -1,48 +1,74 @@
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.Gson;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
+import java.net.*;
+import java.io.*;
+
 
 public class MeetupRSVPProducer {
 
-    public static void main(String[] args) throws Exception{
-//        ws://stream.meetup.com/2/rsvps
+    public static void main(String[] args)  throws IOException{
+
+
         URL url = new URL("https://stream.meetup.com/2/rsvps");
-      InputStream in = url.openStream();
-//
-//        BufferedReader Kn = new BufferedReader(
-//                new InputStreamReader(in,"UTF-8"));
-//
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                conn.getInputStream()));
 
-//        JsonFactory factory = new JsonFactory();
-//        JsonParser jParser = factory.createParser(in);
-////        String line = Kn.readLine();
-//        while (!jParser.isClosed()) {
-//            System.out.println(jParser.readValueAsTree());
-//
-//        }
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
 
-
-            Gson json = new Gson();
-            JsonReader parser = new JsonReader(new InputStreamReader(in));
-                      parser.setLenient(true);
-                     String a =  json.fromJson(parser,String.class);
-                     System.out.println(a);
-//
-//            JsonElement rootelement =  parser.
-//            JsonObject rootobject = rootelement.getAsJsonObject();
 
 
     }
-
-
-
-
 }
+
+
+
+//import com.google.gson.JsonElement;
+//import com.google.gson.JsonObject;
+//import com.google.gson.JsonParser;
+//import com.google.gson.stream.JsonReader;
+//import com.google.gson.Gson;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.io.Reader;
+//import java.net.URL;
+//
+//public class MeetupRSVPProducer {
+//
+//    public static void main(String[] args) throws Exception{
+////        ws://stream.meetup.com/2/rsvps
+//        URL url = new URL("https://stream.meetup.com/2/rsvps");
+//      InputStream in = url.openStream();
+////
+////        BufferedReader Kn = new BufferedReader(
+////                new InputStreamReader(in,"UTF-8"));
+////
+//
+////        JsonFactory factory = new JsonFactory();
+////        JsonParser jParser = factory.createParser(in);
+//////        String line = Kn.readLine();
+////        while (!jParser.isClosed()) {
+////            System.out.println(jParser.readValueAsTree());
+////
+////        }
+//
+//
+//            Gson json = new Gson();
+//            JsonReader parser = new JsonReader(new InputStreamReader(in));
+//                      parser.setLenient(true);
+//                     String a =  json.fromJson(parser,String.class);
+//                     System.out.println(a);
+////
+////            JsonElement rootelement =  parser.
+////            JsonObject rootobject = rootelement.getAsJsonObject();
+//
+//
+//    }
+//
+//
+//
+//
+//}
--- src/test/java/MeetupRSVP.java
+++ src/test/java/MeetupRSVP.java
@@ -1,46 +1,51 @@
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-
-
-public class MeetupRSVP {
-    public static void main(String[] args) throws Exception{
-//        ws://stream.meetup.com/2/rsvps
-        URL url = new URL("https://stream.meetup.com/2/rsvps");
-        InputStream in = url.openStream();
-
+//import com.google.gson.Gson;
+//import com.google.gson.stream.JsonReader;
 //
-        InputStreamReader Kn = new InputStreamReader(in,"UTF-8");
-//        BufferedReader bn = new BufferedReader(kn);
+//import java.io.BufferedReader;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.net.URL;
+//
+//
+//
+//
+//
+//public class MeetupRSVP {
+//    public static void main(String[] args) throws Exception{
+////        ws://stream.meetup.com/2/rsvps
+//        URL url = new URL("https://api.meetup.com/2/rsvps.json");
+//        InputStream in = url.openStream();
+////        JsonParser parser = Json.createParser(new StringReader("[]"));
+//
+////
+//        InputStreamReader Kn = new InputStreamReader(in,"UTF-8");
+//
 //        String line;
-//        while(line =)
+//        while(line == readlines())
+////        while(line =)
+////
 //
-
-//        JsonFactory factory = new JsonFactory();
-//        JsonParser jParser = factory.createParser(in);
-////        String line = Kn.readLine();
-//        while (!jParser.isClosed()) {
-//            System.out.println(jParser.readValueAsTree());
+////        JsonFactory factory = new JsonFactory();
+////        JsonParser jParser = factory.createParser(in);
+//////        String line = Kn.readLine();
+////        while (!jParser.isClosed()) {
+////            System.out.println(jParser.readValueAsTree());
+////
+////        }
 //
-//        }
-
-
-//        Gson json = new Gson();
-//        JsonReader parser = new JsonReader(new InputStreamReader(in));
-//        parser.setLenient(true);
-//        String a =  json.fromJson(parser,String.class);
-//        System.out.println(a);
 //
-//            JsonElement rootelement =  parser.
-//            JsonObject rootobject = rootelement.getAsJsonObject();
-
-
-    }
-
-
-
-}
+////        Gson json = new Gson();
+////        JsonReader parser = new JsonReader(new InputStreamReader(in));
+////        parser.setLenient(true);
+////        String a =  json.fromJson(parser,String.class);
+////        System.out.println(a);
+////
+////            JsonElement rootelement =  parser.
+////            JsonObject rootobject = rootelement.getAsJsonObject();
+//
+//
+//    }
+//
+//
+//
+//}
