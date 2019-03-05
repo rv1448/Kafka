9e7ce4f026cf86917e8828ceda3c0feb5a02f567
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-03-04 16:33:48 -0800

    Meetup producer JSON Stream

--- src/main/java/Kafkaproducerwithcallback.java
+++ src/main/java/Kafkaproducerwithcallback.java
@@ -1,31 +1,71 @@
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class Kafkaproducerwithcallback {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Logger logger = LoggerFactory.getLogger(Kafkaproducerwithcallback.class);
+
+
         Properties properties = new Properties();
-        // Kafka Bootstrap Server
-        properties.setProperty("bootstrap.servers", "192.168.99.100:9092");
-        // Producer acks
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.99.100:9092");
         properties.setProperty("acks", "1");
-        // retries
         properties.setProperty("retries", "3");
-        // Key Serializer
         properties.setProperty("key.serializer", StringSerializer.class.getName());
-        // Value Serializer
         properties.setProperty("value.serializer", StringSerializer.class.getName());
         properties.setProperty("linger.ms", "1");
-        Producer<String, String> producer = new KafkaProducer<String,String>(properties);
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
 
-       for(int i=0; i < 20; i++){
-            producer.send(new ProducerRecord<String, String>("first_topic", Integer.toString(i),Integer.toString(i)));
+        for (int i = 0; i < 10; i++) {
+
+            String topic = "first_topic";
+            String value = "Hello World "+Integer.toString(i);
+            String key = "id_"+Integer.toString(i);
+
+
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key,value);
+            logger.info("Key is "+key);
+
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // Executes every time record is successfully send
+                    if (e == null) {
+                        logger.info("received new Metadata \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp() + "\n"
+
+
+                        );
+                    } else {
+                        logger.error("Error while produced", e);
+                    }
+                }
+            }).get();
         }
+        producer.flush();
         producer.close();
+
+
+//        ProducerRecord<String,String> record = new ProducerRecord<String, String>(topic:);
+
+
+//        Producer<String, String> producer = new KafkaProducer<String,String>(properties);
+
+//
+//       for(int i=0; i < 20; i++){
+//            producer.send(new ProducerRecord<String, String>("first_topic", Integer.toString(i),Integer.toString(i)));
+//        }
+//        producer.close();
     }
 
 }
